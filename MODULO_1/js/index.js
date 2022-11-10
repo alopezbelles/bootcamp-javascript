6410b5e1200ede53965d452f0f0571f6c608906b
alopezbelles <alopezbelles@gmail.com>
2022-11-10 18:35:03 +0100

    first commit

---
+++ MODULO_1/js/index.js
@@ -0,0 +1,92 @@
+
+// EJERCICIO_1
+// console.log('Hola mundo!');
+
+//EJERCICIO_2
+// let nombre = 'alejandro';
+// let edad = 31;
+// let mayorDeEdad = true;
+// let dirección = {calle: 'Barrachina', numero: 77, piso: 1, viviendo:true};
+// let coloresFavoritos = ["azul","verde","naranja","negro"];
+// let lenguajesFavoritos = ["HTML", "CSS","JS"];
+// let mejorLenguaje = lenguajesFavoritos[0];
+// let peorLenguaje = lenguajesFavoritos[2];
+
+// EJERCICIO_3
+// let num1 = 5;
+// let num2 = 10;
+
+// let suma = num1+num2
+// let resta = num2-num1
+// let multiplicacion = num1*num2
+// let division = num2/num1
+
+// console.log(division)
+
+
+// EJERCICIO_4
+
+// let saludo = 'hola';
+
+
+// if (saludo == 'hola') {
+//     console.log("Ha saludado")
+// } else {
+//     console.log("No ha saludado")
+// };
+
+
+//EJERCICIO_5
+
+// let pokemon = ["pickachu","bulbasaur","squertle"];
+
+// console.log(pokemon[0]);
+// console.log(pokemon[1]);
+// console.log(pokemon[2])
+// console.log(pokemon.join())
+
+
+//EJERCICIO_6
+
+
+
+let pockemon = [
+    {
+        nombre: 'pickachu',
+        tipo: "electrico"
+    },
+
+    {
+        nombre: 'squertle',
+        tipo: "agua"
+    },
+
+    {
+        nombre: 'charmander',
+        tipo: "fuego"
+    }
+
+];
+
+// pockemon.forEach(pokemon => {
+//     if(pockemon.tipo === "fuego") {
+//         console.log(${pockemon.nombre} `es un pockemon de fuego`);
+//     };
+// })
+
+
+
+
+
+
+//ESTO ES UNA FORMA DE HACERLO
+// let queTipoDePockemonEs = pockemon[2].tipo
+
+// if (queTipoDePockemonEs === "fuego") {
+//     console.log('Es un pokemon de fuego')
+// } else {
+//     console.log('Es otro tipo de pockemon')
+
+// };
+
+
