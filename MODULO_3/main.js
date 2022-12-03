1cf8d2287a963f05e53dec6aeb48e1b5a4ad039a
alopezbelles <alopezbelles@gmail.com>
2022-12-03 19:28:17 +0100

    modulo 3

---
+++ MODULO_3/main.js
@@ -0,0 +1,44 @@
+//BUCLES
+
+//WHILE
+
+// let i = 0
+
+// while (i <= 10) {
+//   console.log(i)
+//   i++
+// }
+
+
+//DO WHILE
+
+// let i = 0
+
+// do {
+//   console.log(i)
+//   i++
+// } while (i <= 10)
+
+
+//FOR
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i)
+// }
+
+
+// FOR IN 
+
+// const persona = {
+//   nombre: 'Pepe',
+//   edad: 23,
+//   ciudad: 'Madrid'
+// }
+
+// for (const propiedad in persona) {
+//   console.log(propiedad) // nombre, edad, ciudad
+//   console.log(persona[propiedad]) // Pepe, 23, Madrid
+// }
+
+
+Array.reduce()
